c9c2f06cef156506922a19a2a4c3a8043b865eb8
Michael <michael.feyaerts@gmail.com>
2020-01-19 19:06:05 +0100

    init project + decreten query + visualisatie data query

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {DecreetComponent} from './components/decreet/decreet.component';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DecreetComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
