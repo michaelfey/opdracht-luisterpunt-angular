90bfea7fa0bcd2de95babb207c10e71ac871fe98
Michael <michael.feyaerts@gmail.com>
2020-01-20 19:35:55 +0100

    unescape opschrift value in response

--- src/app/components/decreet/decreet.component.ts
+++ src/app/components/decreet/decreet.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import {DecreetService} from "../../services/decreet.service";
+import {Component, OnInit} from '@angular/core';
+import {DecreetService} from '../../services/decreet.service';
 
 @Component({
   selector: 'decreet',
@@ -11,14 +11,19 @@ export class DecreetComponent implements OnInit {
   headers: string[] = [];
   dataResult: any[] = [];
 
-  constructor(private decreetService: DecreetService) { }
+  constructor(private decreetService: DecreetService) {
+  }
 
   ngOnInit() {
     this.decreetService.getLastFiveDecreten().subscribe((result) => {
-      //console.log(result);
       this.headers = result.head.vars;
       this.dataResult = result.results.bindings;
     });
+  }
+
+  unescapeString(str: string) {
+    console.log(decodeURI(str));
+    return decodeURI(str);
   }
 
 }
--- src/app/services/decreet.service.ts
+++ src/app/services/decreet.service.ts
@@ -14,13 +14,10 @@ export class DecreetService {
     "?decreten <http://data.europa.eu/eli/ontology#is_realized_by> ?verschijningsvorm.\n" +
     "?verschijningsvorm <http://data.europa.eu/eli/ontology#title> ?opschrift.\n" +
     "?verschijningsvorm  <http://data.europa.eu/eli/ontology#date_publication> ?publicatieDatum.\n" +
-    "?verschijningsvorm <http://data.europa.eu/eli/ontology#first_date_entry_in_force> ?entrydate.\n" +
-    "\n" +
     "\n" +
     "\n" +
     "}ORDER BY DESC(?publicatieDatum)  LIMIT 5";
 
-  private queryDecretenUri = '?default-graph-uri=https%3A%2F%2Fdata.vlaanderen.be%2Fns%2Fwetgeving&query=PREFIX+rdf%3A+%3Chttp%3A%2F%2Fwww.w3.org%2F1999%2F02%2F22-rdf-syntax-ns%23%3E+PREFIX+rdfs%3A+%3Chttp%3A%2F%2Fwww.w3.org%2F2000%2F01%2Frdf-schema%23%3E+PREFIX+eli%3A+%3Chttp%3A%2F%2Fdata.europa.eu%2Feli%2Fontology%23%3E%0D%0A%0D%0ASELECT+%3Fopschrift+%3FpublicatieDatum+WHERE+%7B%0D%0A%3Fdecreten+%3Chttp%3A%2F%2Fdata.europa.eu%2Feli%2Fontology%23type_document%3E+%3Chttps%3A%2F%2Fdata.vlaanderen.be%2Fid%2Fconcept%2FAardWetgeving%2FDecreet%3E.%0D%0A%0D%0A%3Fdecreten+%3Chttp%3A%2F%2Fdata.europa.eu%2Feli%2Fontology%23is_realized_by%3E+%3Fverschijningsvorm.%0D%0A%3Fverschijningsvorm+%3Chttp%3A%2F%2Fdata.europa.eu%2Feli%2Fontology%23title%3E+%3Fopschrift.%0D%0A%3Fverschijningsvorm++%3Chttp%3A%2F%2Fdata.europa.eu%2Feli%2Fontology%23date_publication%3E+%3FpublicatieDatum.%0D%0A%3Fverschijningsvorm+%3Chttp%3A%2F%2Fdata.europa.eu%2Feli%2Fontology%23first_date_entry_in_force%3E+%3Fentrydate.%0D%0A%0D%0A%0D%0A%0D%0A%7DORDER+BY+DESC%28%3FpublicatieDatum%29++LIMIT+5';
   constructor(private http: HttpClient) {
   }
 
@@ -30,11 +27,10 @@ export class DecreetService {
       .set('Accept', 'application/sparql-results+json');
 
     const params = new HttpParams()
-      //.set('default-graph-uri', this.defaultGraphUri)
-      //.set('query', this.testLastFiveDecretenEncoded);
-      .set('default-graph-uri', encodeURI('https://data.vlaanderen.be/ns/wetgeving'))
-      .set('query', encodeURI(this.lastFiveDecretenQuery));
-    return this.http.get(baseUrl + this.queryDecretenUri, {headers});
+      .set('default-graph-uri', 'https://data.vlaanderen.be/ns/wetgeving')
+      .set('query', this.lastFiveDecretenQuery);
+
+    return this.http.get(baseUrl, {params, headers});
 
   }
 }
